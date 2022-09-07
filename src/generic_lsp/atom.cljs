ab5d0a62ef9459392758c9549386790a06c31adf
Maurício Szabo <mauricio.szabo@gmail.com>
2022-09-07 19:16:04 -0300

    Atom commands

--- src/generic_lsp/atom.cljs
+++ src/generic_lsp/atom.cljs
@@ -1,13 +1,23 @@
 (ns generic-lsp.atom
-  (:require ["atom" :refer [CompositeDisposable]]))
+  (:require ["atom" :refer [CompositeDisposable]]
+            ["url" :as url]))
 
 (defonce subscriptions (atom (CompositeDisposable.)))
 
 (defn info! [message]
-  (.. js/atom -notifications (addInfo message)))
+  (.. js/atom -notifications (addInfo message))
+  nil)
 
 (defn error! [message]
-  (.. js/atom -notifications (addError message)))
+  (.. js/atom -notifications (addError message))
+  nil)
 
 (defn warn! [message]
-  (.. js/atom -notifications (addWarning message)))
+  (.. js/atom -notifications (addWarning message))
+  nil)
+
+(defn open-editor [uri line column]
+  (let [file-name (url/fileURLToPath uri)
+        position (clj->js (cond-> {:initialLine line :searchAllPanes true}
+                                  column (assoc :initialColumn column)))]
+    (.. js/atom -workspace (open file-name position))))
