77703f94a798eec1befde6244b658901e5789f61
Maurício Szabo <mauricio.szabo@gmail.com>
2022-09-08 00:03:32 -0300

    Autocomplete!

--- src/generic_lsp/complete.cljs
+++ src/generic_lsp/complete.cljs
@@ -1,7 +1,39 @@
-(ns generic-lsp.complete)
+(ns generic-lsp.complete
+  (:require [promesa.core :as p]
+            [generic-lsp.commands :as cmds]))
 
-(defn- suggestions [data]
-  (prn :suggestions data))
+; {:editor #object[TextEditor [object Object]],
+;  :bufferPosition #object[Point (2, 2)],
+;  :scopeDescriptor #object[ScopeDescriptor .source.clojure .meta.symbol.clojure],
+;  :prefix "su",
+;  :activatedManually true
+
+(defn- get-prefix! [^js editor]
+  (let [^js cursor (-> editor .getCursors first)
+        start-of-word (-> cursor
+                          (.getBeginningOfCurrentWordBufferPosition #js {:wordRegex #"[^\s]*"})
+                          .-column)
+        current-row (.getBufferRow cursor)
+        current-column (.getBufferColumn cursor)]
+    (when (< start-of-word current-column)
+      (.getTextInBufferRange editor #js [#js [current-row start-of-word]
+                                         #js [current-row current-column]]))))
+
+(defn- suggestions [^js data]
+  (p/let [editor (.-editor data)
+          results (cmds/autocomplete editor)
+          prefix (get-prefix! editor)]
+    (->> results
+         :result
+         (map (fn [result]
+                {:text (:label result)
+                 :type (:kind result)
+                 :replacementPrefix prefix}))
+         not-empty
+         clj->js)))
+
+#_
+:foo
 
 (defn- detailed-suggestion [_data])
   ; (prn :detailed data))
@@ -18,6 +50,6 @@
        :filterSuggestions true
 
        :getSuggestions (fn [data]
-                         (-> data (js->clj :keywordize-keys true) suggestions clj->js))
+                         (suggestions data))
 
        :getSuggestionDetailsOnSelect #(detailed-suggestion %)})
